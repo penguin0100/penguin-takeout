c475dcf10f4f216d3a674f7527559566a7d5e10c
ssz159 <moyuba_159@qq.com>
2026-03-26 19:08:23 +0800

    部分问题 及地址接口

---
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -0,0 +1,38 @@
+package com.sky.service;
+
+import com.sky.dto.*;
+import com.sky.result.PageResult;
+import com.sky.vo.*;
+
+public interface OrderService {
+
+    /**
+     * 用户下单
+     * @param ordersSubmitDTO
+     * @return
+     */
+    OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO);
+
+    /**
+     * 订单支付
+     * @param ordersPaymentDTO
+     * @return
+     */
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception;
+
+    /**
+     * 支付成功，修改订单状态
+     * @param outTradeNo
+     */
+    void paySuccess(String outTradeNo);
+
+    PageResult pageQuery4User(int page, int pageSize, Integer status);
+
+    OrderVO details(Long id);
+
+    void userCancelById(Long id);
+
+    void repetition(Long id);
+
+    void reminder(Long id);
+}
